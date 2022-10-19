c0b6c1b64f6eb1a1d58581d7f1330b9fa030e472
black143 <59355045+black-143@users.noreply.github.com>
2022-10-19 18:59:35 +0530

    Add files via upload

---
+++ FiveStarRating/FiveStarRating/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./styles.css";
+import { FaStar } from "react-icons/fa";
+
+export default function App() {
+  const [rating, setRating] = useState();
+  const [hover, setHover] = useState();
+  return (
+    <div className="App">
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+
+        return (
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            <FaStar
+              className="star"
+              color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+              size={100}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
+            />
+          </label>
+        );
+      })}
+    </div>
+  );
+}
