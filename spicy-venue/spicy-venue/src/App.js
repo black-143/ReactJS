25182096f1ecb201e88b58a8ee5afd8ffdc6e268
black143 <59355045+black-143@users.noreply.github.com>
2022-11-07 21:59:38 +0530

    Add files via upload

---
+++ spicy-venue/spicy-venue/src/App.js
@@ -0,0 +1,10 @@
+import tokenCounter from "./components/tokenCounter";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <tokenCounter />
+    </div>
+  );
+}
---
+++ spicy-venue/spicy-venue/src/components/tokenCounter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./tokenCounter.css";
+
+const tokenCounter = () => {
+  return (
+    <div>
+      <h2 className="spicy">The Spicy Venue</h2>
+      <div className="parent">
+        <div className="four">4</div>
+      </div>
+      <p>Running Token No</p>
+      <div className="parent1">
+        <div className="five"> 5</div>
+
+        <p>Your Token No</p>
+      </div>
+    </div>
+  );
+};
+export default tokenCounter;
