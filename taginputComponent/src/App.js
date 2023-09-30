915b2889f4e2aab5a9a9d7d01d8346d3a1c63eeb
Pravallika Yakkala <59355045+black-143@users.noreply.github.com>
2023-09-30 12:35:40 +0530

    Add files via upload

---
+++ taginputComponent/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import TagInput from "./components/TagInput";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TagInput />
+    </div>
+  );
+}
