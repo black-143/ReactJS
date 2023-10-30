a16fa6f55f2235f48af3aa4fe68168a210cb2ba9
Pravallika Yakkala <59355045+black-143@users.noreply.github.com>
2023-10-30 08:35:03 +0530

    Add files via upload

---
+++ useReducer-counter/src/App.js
@@ -0,0 +1,10 @@
+import CounterReducer from "./CounterReducer";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CounterReducer />
+    </div>
+  );
+}
