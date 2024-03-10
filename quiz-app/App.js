5f944148f6696a7b4507ee739451dd5646e3948a
Pravallika Yakkala <59355045+black-143@users.noreply.github.com>
2024-03-10 12:23:58 +0530

    Add files via upload

---
+++ quiz-app/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import questions from "./constants/questions.json";
+import Question from "./components/question";
+import Result from "./components/result";
+import "./styles.css";
+function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  // Keep all of the logic in App.jsx
+
+  const handleNextQuestion = (isCorrect) => {
+    setCurrentQuestion(currentQuestion + 1);
+    setUserAnswers([...userAnswers, isCorrect]);
+  };
+
+  const resetQuiz = () => {
+    setCurrentQuestion(0);
+    setUserAnswers([]);
+  };
+
+  return (
+    <div className="App">
+      <h1>World Quiz</h1>
+
+      {/* Questions Component */}
+      {currentQuestion < questions.length && (
+        <Question
+          question={questions[currentQuestion]}
+          onAnswerClick={handleNextQuestion}
+        />
+      )}
+
+      {/* Result Component */}
+      {currentQuestion === questions.length && (
+        <Result
+          userAnswers={userAnswers}
+          questions={questions}
+          resetQuiz={resetQuiz}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
